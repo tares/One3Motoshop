5963dce5e0901d5eaa37df1c7671f8762c07e584
AE Antares <tarres_way@hotmail.com>
2012-12-04 14:59:53 +0700

    new add

--- content/product.inc.php
+++ content/product.inc.php
@@ -1,32 +1,35 @@
 		<div id="mainSection">
-        	<div id="sectionTOP" class="dropShadow_lighter">
-                <div id="font-headserSub">
-                    Product Catalogue
-                </div>
-                <!--------SEARCH------>
-                <div id="tools-search">
-                    <form action="#" method="post" class="form-wrapper cf">
-                        <div>
-                              <input name="search" type="text"/>
-                              <button type="submit">Search</button>
-                        </div>
-                    </form>
-                </div>
-                 <!-------------------->
-			</div>
+        	<div id="searchBOX"></div>
         	<div id="productWrapper">
             	<div id="section-left" class="dropShadow_lighter"></div>
                 <div id="section-right" class="dropShadow_lighter">
-                	<div id="product-box-row">
-                    	<div id="eventWrapper">
-                            <center>
-                                <a href="#Ducati"><img id="brand_picThumb" class="blur" src="source/images/product/brand/ducati120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                <a href="#Honda"><img id="brand_picThumb" class="blur" src="source/images/product/brand/honda120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                <a href="#Kawasaki"><img id="brand_picThumb" class="blur" src="source/images/product/brand/kawasaki120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                <a href="#KTM"><img id="brand_picThumb" class="blur" src="source/images/product/brand/ktm120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                <a href="#Suzuki"><img id="brand_picThumb" class="blur" src="source/images/product/brand/suzuki120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                <a href="#Yamaha"><img id="brand_picThumb" class="blur" src="source/images/product/brand/yamaha120x120.png" width="120" height="120" alt="Brand Logo" /></a>       
-                            </center>
+                	<div id="sectionTOP" class="dropShadow_lighter">
+                        <div id="font-headserSub">
+                            Product Catalogue
+                        </div>
+                        <!--------SEARCH------>
+                        <div id="tools-search">
+                            <form action="#" method="post" class="form-wrapper cf">
+                                <div>
+                                      <input name="search" type="text"/>
+                                      <button type="submit">Search</button>
+                                </div>
+                            </form>
+                        </div>
+                         <!-------------------->
+                    </div>
+                    <div id="paddingWrapper">
+                    	<div id="product-box-row">
+                            <div id="eventWrapper">
+                                <center>
+                                    <a href="#Ducati"><img id="brand_picThumb" class="blur" src="source/images/product/brand/ducati120x120.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Honda"><img id="brand_picThumb" class="blur" src="source/images/product/brand/honda120x120.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Kawasaki"><img id="brand_picThumb" class="blur" src="source/images/product/brand/kawasaki120x120.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#KTM"><img id="brand_picThumb" class="blur" src="source/images/product/brand/ktm120x120.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Suzuki"><img id="brand_picThumb" class="blur" src="source/images/product/brand/suzuki120x120.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Yamaha"><img id="brand_picThumb" class="blur" src="source/images/product/brand/yamaha120x120.png" width="120" height="120" alt="Brand Logo" /></a>       
+                                </center>
+                            </div>
                         </div>
                     </div>
                 </div>
