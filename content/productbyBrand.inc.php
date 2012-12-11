ec30860d9722781c8e10933cf604edfd5435cdb0
AE Antares <tarres_way@hotmail.com>
2012-12-11 17:58:21 +0700

    logo

--- content/productbyBrand.inc.php
+++ content/productbyBrand.inc.php
@@ -56,12 +56,14 @@
                     	<div id="product-box-row">
                             <div id="brandWrapper">
                                 <center>
-                                    <a href="#Ducati"><img id="brand_picThumb" class="blur" src="source/images/product/brand/ducati120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                    <a href="#Honda"><img id="brand_picThumb" class="blur" src="source/images/product/brand/honda120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                    <a href="#Kawasaki"><img id="brand_picThumb" class="blur" src="source/images/product/brand/kawasaki120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                    <a href="#KTM"><img id="brand_picThumb" class="blur" src="source/images/product/brand/ktm120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                    <a href="#Suzuki"><img id="brand_picThumb" class="blur" src="source/images/product/brand/suzuki120x120.png" width="120" height="120" alt="Brand Logo" /></a>
-                                    <a href="#Yamaha"><img id="brand_picThumb" class="blur" src="source/images/product/brand/yamaha120x120.png" width="120" height="120" alt="Brand Logo" /></a>       
+                                    <a href="#Ducati"><img id="brand_picThumb" class="blur" src="source/images/product/product/akrapovic.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Honda"><img id="brand_picThumb" class="blur" src="source/images/product/product/arrow.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Kawasaki"><img id="brand_picThumb" class="blur" src="source/images/product/product/ermax.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#KTM"><img id="brand_picThumb" class="blur" src="source/images/product/product/hotbodies.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Suzuki"><img id="brand_picThumb" class="blur" src="source/images/product/product/KnN.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Yamaha"><img id="brand_picThumb" class="blur" src="source/images/product/product/rizoma.png" width="120" height="120" alt="Brand Logo" /></a>
+                                    <a href="#Yamaha"><img id="brand_picThumb" class="blur" src="source/images/product/product/RnG.png" width="120" height="120" alt="Brand Logo" /></a>       
+                                    <a href="#Yamaha"><img id="brand_picThumb" class="blur" src="source/images/product/product/twobrother.png" width="120" height="120" alt="Brand Logo" /></a>       
                                 </center>
                             </div>
                         </div>
